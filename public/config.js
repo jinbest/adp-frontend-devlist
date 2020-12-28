cdc460f2b7453966cdbed10c6911bf2ccbee0430
Ehidiamen Osarhemen <eosarhemen@gmail.com>
2020-12-27 21:07:25 -0600

    setup config

---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.REACT_APP_STORE_SERVICE_API_URL = "http://dev.dccmtx.com/api/store-service/"
+window.REACT_APP_ADMIN_SERVICE_API_URL = "http://dev.dccmtx.com/api/administration-service/"
+window.REACT_APP_PRODUCT_SERVICE_API_URL = "http://dev.dccmtx.com/api/product-service/"
+window.REACT_APP_REPAIR_SERVICE_API_URL = "http://dev.dccmtx.com/api/repair-service/"
+window.REACT_APP_ENVIRONMENT = "local"
