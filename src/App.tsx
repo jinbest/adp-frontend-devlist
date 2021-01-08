ce686e37528a88f12daaacce1f83af8e9ca8566b
jinbest <jin.bestvictoria718@gmail.com>
2021-01-08 23:34:08 +0800

    geebo homepage, repair-page, and repair-widget

--- src/App.tsx
+++ src/App.tsx
@@ -9,50 +9,50 @@ import store from "./store/RepairWidgetStore"
 /* eslint-disable */
 // const domainMatch = window.location.hostname.match(/[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*$/)
 // const subDomain = domainMatch ? domainMatch[0].split(".")[0] : "localhost"
-const devicelist = ['dccmtx', 'devicelist', 'mtlcmtx', 'northtech']
-const subDomain = devicelist[0]
+const devicelist = ['dccmtx', 'devicelist', 'geebo', 'mtlcmtx', 'northtech']
+const subDomain = devicelist[2]
 
 function App(): JSX.Element {
-    require(`./assets/${subDomain}/styles/index.css`)
+  require(`./assets/${subDomain}/styles/index.css`)
 
-    const [footerStatus, setFooterStatus] = useState(true)
+  const [footerStatus, setFooterStatus] = useState(true)
 
-    const handleFooterStatus = (status: boolean) => {
-        setFooterStatus(status)
-    }
+  const handleFooterStatus = (status: boolean) => {
+    setFooterStatus(status)
+  }
 
-    const BaseRouter = () => {
-        return (
-            <>
-                <Route path="/" exact component={() => <Home subDomain={subDomain} />} />
-                <Route path="/home" render={() => <Redirect to="/" />} />
-                <Route
-                    path="/repair"
-                    component={() => (
-                        <Provider repairWidgetStore={store}>
-                            <Repair subDomain={subDomain} handleStatus={handleFooterStatus} />
-                        </Provider>
-                    )}
-                />
-                <Route
-                    path="/repair-widget"
-                    component={() => (
-                        <Provider repairWidgetStore={store}>
-                            <RepairWidget subDomain={subDomain} handleStatus={handleFooterStatus} />
-                        </Provider>
-                    )}
-                />
-            </>
-        )
-    }
-
+  const BaseRouter = () => {
     return (
-        <Router>
-            <Header subDomain={subDomain} handleStatus={handleFooterStatus} />
-            <BaseRouter />
-            {footerStatus && <Footer subDomain={subDomain} />}
-        </Router>
+      <>
+        <Route path="/" exact component={() => <Home subDomain={subDomain} />} />
+        <Route path="/home" render={() => <Redirect to="/" />} />
+        <Route
+          path="/repair"
+          component={() => (
+            <Provider repairWidgetStore={store}>
+              <Repair subDomain={subDomain} handleStatus={handleFooterStatus} />
+            </Provider>
+          )}
+        />
+        <Route
+          path="/repair-widget"
+          component={() => (
+            <Provider repairWidgetStore={store}>
+              <RepairWidget subDomain={subDomain} handleStatus={handleFooterStatus} />
+            </Provider>
+          )}
+        />
+      </>
     )
+  }
+
+  return (
+    <Router>
+      <Header subDomain={subDomain} handleStatus={handleFooterStatus} />
+      <BaseRouter />
+      {footerStatus && <Footer subDomain={subDomain} />}
+    </Router>
+  )
 }
 
 export default App
