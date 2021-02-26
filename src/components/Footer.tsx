684883659462b84ddb0210746fa380363ef73afe
jinbest <jin.bestvictoria718@gmail.com>
2021-02-27 04:05:43 +0800

    Main vender should be displayed at fist on footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -46,14 +46,6 @@ const useStyles = makeStyles(() =>
   })
 )
 
-type StoreProps = {
-  headerStore: StoresDetails
-}
-interface Props extends StoreProps {
-  subDomain?: string
-  features: any[]
-}
-
 type GridMDInterface =
   | boolean
   | 2
@@ -71,6 +63,149 @@ type GridMDInterface =
   | 12
   | undefined
 
+type FooterLinksComponentProps = {
+  data: any[]
+  isMain: boolean
+  initGridMD: GridMDInterface
+  subDomain?: string
+}
+
+const FooterLinksComponent = ({
+  data,
+  isMain,
+  initGridMD,
+  subDomain,
+}: FooterLinksComponentProps) => {
+  const classes = useStyles()
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
+  const open = Boolean(anchorEl)
+
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <React.Fragment>
+      {data.map((item: any, index: number) => {
+        return (
+          <React.Fragment key={index}>
+            {item.is_main === isMain && (
+              <Grid item xs={12} sm={initGridMD}>
+                <Typography
+                  aria-owns={open ? "mouse-over-popover" : undefined}
+                  aria-haspopup="true"
+                  onMouseEnter={handlePopoverOpen}
+                  onMouseLeave={handlePopoverClose}
+                  className={subDomain + "-footer-subContent-title " + classes.footerLocName}
+                >
+                  <a
+                    href={
+                      item.business_page_link ||
+                      `https://www.google.com/maps/search/?api=1&query=${getAddress(item)
+                        .split(" ")
+                        .join("+")}`
+                    }
+                    className={classes.hoverEffect}
+                    style={{ color: "black" }}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {item.location_name}
+                  </a>
+                </Typography>
+                <div
+                  className={subDomain + "-device-list-grid"}
+                  style={{ marginTop: "5px", flexWrap: "wrap" }}
+                >
+                  <a
+                    href={`tel:${item.phone}`}
+                    style={{ color: "black", whiteSpace: "nowrap" }}
+                    className={classes.hoverEffect}
+                  >
+                    {phoneFormatString(item.phone)} |
+                  </a>
+                  &nbsp;
+                  <a
+                    href={`mailto:${item.email}`}
+                    style={{ color: "black", whiteSpace: "nowrap" }}
+                    className={classes.hoverEffect}
+                  >
+                    {item.email}
+                  </a>
+                </div>
+                <Typography
+                  aria-owns={open ? "mouse-over-popover" : undefined}
+                  aria-haspopup="true"
+                  onMouseEnter={handlePopoverOpen}
+                  onMouseLeave={handlePopoverClose}
+                  className={subDomain + "-device-list-grid " + classes.footerLocAddress}
+                >
+                  <a
+                    href={
+                      item.business_page_link ||
+                      `https://www.google.com/maps/search/?api=1&query=${getAddress(item)
+                        .split(" ")
+                        .join("+")}`
+                    }
+                    style={{ color: "black" }}
+                    className={classes.hoverEffect}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {(item.address_1 ? item.address_1 + ", " : "") +
+                      (item.address_2 ? item.address_2 + ", " : "") +
+                      (item.city ? item.city + ", " : "") +
+                      (item.state ? item.state + ", " : "") +
+                      (item.postcode
+                        ? item.postcode.substring(0, 3) +
+                          " " +
+                          item.postcode.substring(3, item.postcode.length)
+                        : "")}
+                  </a>
+                </Typography>
+                <Popover
+                  id="mouse-over-popover"
+                  className={classes.popover}
+                  classes={{
+                    paper: classes.paper,
+                  }}
+                  open={open}
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: -5,
+                    horizontal: "left",
+                  }}
+                  transformOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  onClose={handlePopoverClose}
+                  disableRestoreFocus
+                >
+                  <Typography className={classes.popovertext}>
+                    Find address on Google maps.
+                  </Typography>
+                </Popover>
+              </Grid>
+            )}
+          </React.Fragment>
+        )
+      })}
+    </React.Fragment>
+  )
+}
+
+type StoreProps = {
+  headerStore: StoresDetails
+}
+interface Props extends StoreProps {
+  subDomain?: string
+  features: any[]
+}
+
 const Footer = inject("headerStore")(
   observer((props: Props) => {
     const { subDomain, features, headerStore } = props
@@ -79,21 +214,8 @@ const Footer = inject("headerStore")(
     const gridVal = data.homeTextData.footer.gridVal
     const t = useT()
 
-    const classes = useStyles()
-
     const [feats, setFeatures] = useState<any[]>([])
     const [initGridMD, setInitGridMD] = useState<GridMDInterface>(12)
-    const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
-
-    const handlePopoverOpen = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-      setAnchorEl(event.currentTarget)
-    }
-
-    const handlePopoverClose = () => {
-      setAnchorEl(null)
-    }
-
-    const open = Boolean(anchorEl)
 
     useEffect(() => {
       const cntFeatures: any[] = []
@@ -136,105 +258,15 @@ const Footer = inject("headerStore")(
                 }}
               />
               <Grid item container xs={12} md={12}>
-                {headerStore.allLocations.map((item: any, index: number) => {
+                {[true, false].map((item: any, index: number) => {
                   return (
-                    <Grid item xs={12} sm={initGridMD} key={index}>
-                      <Typography
-                        aria-owns={open ? "mouse-over-popover" : undefined}
-                        aria-haspopup="true"
-                        onMouseEnter={handlePopoverOpen}
-                        onMouseLeave={handlePopoverClose}
-                        className={subDomain + "-footer-subContent-title " + classes.footerLocName}
-                      >
-                        <a
-                          href={
-                            item.business_page_link ||
-                            `https://www.google.com/maps/search/?api=1&query=${getAddress(item)
-                              .split(" ")
-                              .join("+")}`
-                          }
-                          className={classes.hoverEffect}
-                          style={{ color: "black" }}
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          {item.location_name}
-                        </a>
-                      </Typography>
-                      <div
-                        className={subDomain + "-device-list-grid"}
-                        style={{ marginTop: "5px", flexWrap: "wrap" }}
-                      >
-                        <a
-                          href={`tel:${item.phone}`}
-                          style={{ color: "black", whiteSpace: "nowrap" }}
-                          className={classes.hoverEffect}
-                        >
-                          {phoneFormatString(item.phone)} |
-                        </a>
-                        &nbsp;
-                        <a
-                          href={`mailto:${item.email}`}
-                          style={{ color: "black", whiteSpace: "nowrap" }}
-                          className={classes.hoverEffect}
-                        >
-                          {item.email}
-                        </a>
-                      </div>
-                      <Typography
-                        aria-owns={open ? "mouse-over-popover" : undefined}
-                        aria-haspopup="true"
-                        onMouseEnter={handlePopoverOpen}
-                        onMouseLeave={handlePopoverClose}
-                        className={subDomain + "-device-list-grid " + classes.footerLocAddress}
-                      >
-                        <a
-                          href={
-                            item.business_page_link ||
-                            `https://www.google.com/maps/search/?api=1&query=${getAddress(item)
-                              .split(" ")
-                              .join("+")}`
-                          }
-                          style={{ color: "black" }}
-                          className={classes.hoverEffect}
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          {(item.address_1 ? item.address_1 + ", " : "") +
-                            (item.address_2 ? item.address_2 + ", " : "") +
-                            (item.city ? item.city + ", " : "") +
-                            (item.state ? item.state + ", " : "") +
-                            (item.postcode
-                              ? item.postcode.substring(0, 3) +
-                                " " +
-                                item.postcode.substring(3, item.postcode.length)
-                              : "")}
-                        </a>
-                      </Typography>
-                      <Popover
-                        id="mouse-over-popover"
-                        className={classes.popover}
-                        classes={{
-                          paper: classes.paper,
-                        }}
-                        open={open}
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                          vertical: -5,
-                          horizontal: "left",
-                        }}
-                        transformOrigin={{
-                          vertical: "bottom",
-                          horizontal: "left",
-                        }}
-                        onClose={handlePopoverClose}
-                        disableRestoreFocus
-                      >
-                        <Typography className={classes.popovertext}>
-                          Find address on Google maps.
-                        </Typography>
-                      </Popover>
-                    </Grid>
+                    <FooterLinksComponent
+                      key={index}
+                      data={headerStore.allLocations}
+                      isMain={item}
+                      subDomain={subDomain}
+                      initGridMD={initGridMD}
+                    />
                   )
                 })}
               </Grid>
