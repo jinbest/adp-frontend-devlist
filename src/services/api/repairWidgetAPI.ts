d85c405750bf5933c39f0887f0289585af22da97
Ehidiamen Osarhemen <eosarhemen@gmail.com>
2021-03-05 23:06:41 -0600

    sort repairs by display_order

--- src/services/api/repairWidgetAPI.ts
+++ src/services/api/repairWidgetAPI.ts
@@ -117,7 +117,7 @@ class RepairWidgetAPI {
     include_cost: boolean,
     text: string
   ) => {
-    const apiURL = `${Config.REPAIR_SERVICE_API_URL}dc/${locale}/store/${store_id}/repair?per_page=${per_page}&page=${page}&include_voided=${included_voided}&product_id=${product_id}&is_active=${is_active}&include_cost=${include_cost}&title=${text}&display_order=asc`
+    const apiURL = `${Config.REPAIR_SERVICE_API_URL}dc/${locale}/store/${store_id}/repair?per_page=${per_page}&page=${page}&include_voided=${included_voided}&product_id=${product_id}&is_active=${is_active}&include_cost=${include_cost}&title=${text}&display_sort_order=asc`
     return new Promise((resolve, reject) => {
       axios
         .get(`${apiURL}`)
