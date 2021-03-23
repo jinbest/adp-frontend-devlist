4f9eee0f1c29c627ac778400dde797b089ae8a23
Ehidiamen Osarhemen <eosarhemen@gmail.com>
2021-03-23 11:38:00 -0500

    move translations to backend

--- src/i18-next/i18n.js
+++ src/i18-next/i18n.js
@@ -1,58 +1,16 @@
 import i18n from "i18next"
 import detector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next"
-// import Config from "../config/config"
-// import HttpBackend from "i18next-http-backend"
-
-// i18n
-//   .use(detector)
-//   .use(initReactI18next) // passes i18n down to react-i18next
-//   .use(HttpBackend)
-//   .init({
-//     lng: "en",
-
-//     fallbackLng: "en", // use en if detected lng is not available
-
-//     keySeparator: false, // we do not use keys in form messages.welcome
-
-//     interpolation: {
-//       escapeValue: false, // react already safes from xss
-//     },
-
-//     preload: ["en", "fr"],
-
-//     ns: ["translation"],
-
-//     defaultNS: "translation",
-
-//     backend: {
-//       loadPath: `${Config.TRANSLATION_SERVICE_API_URL}/dc/translation/dashboard/{{lng}}`,
-//     },
-
-//     react: { wait: true, useSuspense: false },
-//   })
-
-// export default i18n
-
-import translationENG from "./en.json"
-import translationFR from "./fr.json"
-
-// the translations
-const resources = {
-  eng: {
-    translation: translationENG,
-  },
-  fr: {
-    translation: translationFR,
-  },
-}
+import Config from "../config/config"
+import HttpBackend from "i18next-http-backend"
 
 i18n
   .use(detector)
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(HttpBackend)
   .init({
-    resources,
     lng: "en",
+
     fallbackLng: "en", // use en if detected lng is not available
 
     keySeparator: false, // we do not use keys in form messages.welcome
@@ -60,6 +18,18 @@ i18n
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
+
+    preload: ["en", "fr"],
+
+    ns: ["translation"],
+
+    defaultNS: "translation",
+
+    backend: {
+      loadPath: `${Config.TRANSLATION_SERVICE_API_URL}dc/translation/frontend/{{lng}}`,
+    },
+
+    react: { wait: true, useSuspense: false },
   })
 
 export default i18n
