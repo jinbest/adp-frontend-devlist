4e1899be0f6095b9dd0fd35ee697c200da778e08
jinbest <jin.bestvictoria718@gmail.com>
2021-04-15 23:22:51 +0800

    Fix button issue for spinner & ellipsis functionality

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -38,8 +38,8 @@ const Button = ({
   disable,
   border,
   textDecorator,
-}: // children,
-Props) => {
+  children,
+}: Props) => {
   return (
     <>
       <button
@@ -64,9 +64,15 @@ Props) => {
         disabled={disable}
       >
         {icon && <RoomOutlinedIcon />}
-        <EllipsisText text={title} length={"20"} />
+        {title && title.length > 20 ? (
+          <EllipsisText text={title} length={20} />
+        ) : children ? (
+          children
+        ) : (
+          title
+        )}
       </button>
-      {title.length > 20 && (
+      {title && title.length > 20 && (
         <ReactTooltip id={title} place="top" effect="solid">
           {title}
         </ReactTooltip>
